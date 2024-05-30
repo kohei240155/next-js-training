a7ebd780c70a275db0259ab739ed6afec0c534da
kohei240155 <k.pino519@gmail.com>
2024-05-30 22:02:00 +0900

    No3 Answer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-import styles from './styles/Home.module.css'
+"use client"
+
+import styles from "./styles/Home.module.css";
 
 export default function Home() {
+
+  const handleClick = () => {
+    alert("ボタンがクリックされました。");
+  };
+
   return (
-    <h1 className={styles.title}>Hello World</h1>
-  )
+    <>
+      <h1 className={styles.title}>Hello World</h1>
+      <button onClick={handleClick}>Click Me</button>
+    </>
+  );
 }
